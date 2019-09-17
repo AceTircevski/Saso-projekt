aa7ee99f619aef203ae9b130ebabdbac18767028
AceTircevski <47758694+AceTircevski@users.noreply.github.com>
2019-09-17 21:52:46 +0200

    commit

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-
+document.addEventListener(
+  "wheel",
+  evt => {
+    var header = document.getElementsByTagName("header")[0];
+    if (window.scrollY > window.innerHeight) {
+      header.classList.add("header-is-hidden");
+    } else {
+      header.classList.remove("header-is-hidden");
+    }
+  },
+  { capture: false, passive: true }
+);
